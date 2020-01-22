6cdcb9f9389d4564c2ef43b5316f46377032c915
YamanKatmawi <54937863+YamanKatmawi@users.noreply.github.com>
2020-01-22 15:29:21 +0100

    Add files via upload

---
+++ book1.js
@@ -0,0 +1,46 @@
+var x = 5;
+var y = 6;
+console.log(x + y);
+console.log(x * y);
+console.log(x % y);
+//////
+var text = "this is fine time";
+console.log(text.slice(3));
+/////
+var text1 = "this is big came";
+console.log(text1.slice(0));
+////
+var text2 = "this is a big match";
+console.log(text2.toLocaleUpperCase());
+////
+var text3 = "THIS is a big came";
+console.log(text3.toLocaleLowerCase(2));
+////
+var str = 1.5;
+var str1 = 11;
+console.log(str + " " + str1);
+console.log(!isNaN(str1) ? "yes it is" : "No it is");
+console.log(!isNaN(str) ? "yes it is" : " No it is");
+/////
+console.log(Math.min(-2, 6, -9, 6, 10));
+console.log(Math.max(555, 99, 647, 998, 321, -33));
+////
+var str2 = "Hi how are you";
+var str3 = "Wie geht es dir";
+console.log(`${str2} ${str3}`);
+//////
+var text4 = "nice came";
+var textCheck = text4.includes("a");
+console.log(textCheck);
+////
+
+///////
+
+console.log(Math.floor(Math.random() * 100) + 1);
+///////
+console.log(Math.floor(Math.random(2) * 100) + 1);
+console.log(Math.ceil(Math.random() * 100) + 1);
+/////
+
+7jYq4y5Sf5K2xX       PASSWOORD
+/////
